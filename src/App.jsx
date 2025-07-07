0db1c747cbe93e6de58301f5ddfa68323839915e
AlexanderPaul1983 <alexanderpaul@gmx.de>
2025-07-07 11:15:03 +0200

    added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Main from './components/Main.jsx'
 
 const App = () => {
   return (
     <div>
-      
+      <h1>App</h1>
+      <Main />
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import styles from "../css.modules/Main.module.css";
+
+function Main() {
+    return (
+        <div className={styles.main}>
+            <h1>Main</h1>
+        </div>
+    );
+}
+
+export default Main;
